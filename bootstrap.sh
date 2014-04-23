4406bb3d4c2664bb53dc16fd294d38f6fe091569
Patrick Mizer <patrick@sparefoot.com>
2014-04-23 10:08:21 -0500

    Add pear and phing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+#update repos
+sudo apt-get update
+
 #install PHP / Apache
-sudo apt-get install -y php5 libapache2-mod-php5
+sudo apt-get install -y php5 libapache2-mod-php5 php-pear
 
 # Install jenkins
 sudo apt-get install -y jenkins
+
+pear channel-discover pear.phing.info
+pear install --alldeps phing/phing
